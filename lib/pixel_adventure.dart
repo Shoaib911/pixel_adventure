a04a37af3730d079fb0dd5ad0a3b1b16facc7b93
Peer Zada Syed Sufiyan Ali <146112375+Sufixxx@users.noreply.github.com>
2023-11-09 23:21:27 +0500

    Add player Animation

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -11,13 +11,17 @@ class PixelAdventure extends FlameGame {
   Color backgroundColor() => const Color(0xFF211F30);
 
   late final CameraComponent cam;
-  final world = Level();
+  final world = Level(levelName: 'Level-02');
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    // Load All Images into Cache
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
         world: world, width: 640, height: 360);
     cam.viewfinder.anchor = Anchor.topLeft;
     addAll([cam, world]);
+
     return super.onLoad();
   }
 }
